b047ef9a2f80fbd81e418b39f5d46809fbc94c8f
haworku <haworku@gmail.com>
2019-09-11 14:56:51 -0500

    Edit about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,23 +15,31 @@ const AboutPage = () => (
     <SEO title="About" />
     <div style={style.textContainer}>
       <p>
-        Who are we building technology for? What assumptions about the future of
-        this technology or its users are we making? I find direction in my
-        projects by considering the actual people that benefit and the stories
-        they tell about what's needed.
+        Who are we building for? What assumptions about the future of technology
+        or its users are we making? I find direction in my projects by
+        considering the actual people that benefit and the stories they tell
+        about what's needed.
       </p>
       <p>
-        I'm a writer and engineer. The majority of my professional work has been
-        as a web developer in Javascript and JS frameworks (React/Angular). At
-        the same time, I enjoy being interdisciplinary - working across
-        languages and the stack as well as working closely with product teams
-        and users.
+        I'm a writer, strategist, and developer. The majority of my professional
+        work in software has been as a web developer in Javascript and JS
+        frameworks (React/Angular). My coworkers say that I’m especially skilled
+        at debugging - taking on complicated work and seeing it through to
+        completion. I think the secret is just being unafraid to ask questions,
+        sometimes a lot of them. Prior to working in tech, I worked in nonprofit
+        and civic organizations - interfacing regularly with young people,
+        elders, policymakers, families, academics, business owners, and so many
+        others. I know what it's like to engage very different kinds of people,
+        who often have different language and understanding around a problem and
+        its solutions. I know how to navigate complexity and still be
+        collaborative.
       </p>
       <p>
-        I appreciate organizations that invest in their own people and internal
-        processes. I'm intrigued by projects that involve working with users and
-        understanding how they think or by projects that involve strategically
-        refactoring and improving a codebase over time.
+        Accordingly, I appreciate workplaces that care about the process of work
+        - how/why we work and not only what we create. I think space for
+        reflection, disagreement, and ethical discussions at the workplace is
+        essential. In this context, I enjoy working across languages and the
+        stack, working closely with product teams and users.
       </p>
     </div>
   </Layout>
